340d7f4f6ed8cbd15227ad11d343193990727dbb
JanRossbach <janrossbach3@gmail.com>
2023-03-11 19:12:56 +0000

    add: Editing password files

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,112 @@
 use clap::{Args, Parser, Subcommand};
 use regex::RegexSet;
-use std::collections::HashSet;
-use std::io::Read;
-use std::{env, fs};
+use rpass::RpassManager;
 
-use std::str::FromStr;
+use std::{str::FromStr, env, process::Command, io::{self, Read}, fs::File};
 
 use home::home_dir;
 
-use gpgme::{Context, Key, KeyListMode, Protocol};
-use std::fs::{create_dir_all, File};
-use std::{io::Write, path::PathBuf};
+use std::path::PathBuf;
+
+mod rpass;
 
 const RPASS_DEFAULT_STORE_NAME: &str = ".rpassword_store";
-const GPG_ID_FILE_NAME: &str = "/.gpg-id";
 
-fn read_gpg_id(store_dir: &PathBuf) -> std::io::Result<String> {
-    let gpg_id_filename = store_dir.to_str().unwrap().to_string() + GPG_ID_FILE_NAME;
-    let mut gpg_id_file = fs::File::open(gpg_id_filename)?;
-    let mut gpg_id = String::new();
-    gpg_id_file.read_to_string(&mut gpg_id)?;
-    return Ok(gpg_id);
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct RpassArgs {
+    #[clap(subcommand)]
+    command: RpassCommand,
 }
 
-fn read_password_names(store_dir: &PathBuf) -> std::io::Result<HashSet<String>> {
-    let mut passwords: HashSet<String> = HashSet::new();
-    for dir_entry in fs::read_dir(store_dir)? {
-        let dir_entry = dir_entry?;
-        let dir_entry = dir_entry.path();
-        if let Some(ext) = dir_entry.extension() {
-            if ext == "gpg" {
-                let file_name = dir_entry.file_stem().unwrap();
-                let file_name = file_name.to_str().unwrap();
-                passwords.insert(file_name.to_string());
-            }
-        }
-    }
-    Ok(passwords)
+#[derive(Debug, Subcommand)]
+enum RpassCommand {
+    /// Initialize new password storage and use gpg-id for encryption.
+    /// Selectively reencrypt existing passwords using new gpg-id.
+    Init(InitCommand),
+    /// List passwords.
+    Ls(LSCommand),
+    /// List passwords that match pass-names.
+    Find(FindCommand),
+    /// Show existing password and optionally put it on the clipboard.
+    Show(ShowCommand),
+    /// Insert new password. Optionally, echo the password back to the console
+    /// during entry. Prompt before overwriting existing password unless forced.
+    Insert(InsertCommand),
+    /// Insert a new password or edit an existing password using your $EDITOR or neovim as a fallback.
+    Edit(EditCommand),
 }
 
-pub fn ls(store_dir: PathBuf) -> std::io::Result<()> {
-    let pw_names = read_password_names(&store_dir)?;
-    println!("Password store:");
-    for pw in pw_names {
-        println!("|-- {}", pw);
-    }
-    Ok(())
+#[derive(Args, Debug)]
+struct InitCommand {
+    #[arg(short, long)]
+    subfolder: Option<String>,
+
+    gpg_id: String,
 }
 
-fn init(gpg_id: &str, store_dir: PathBuf) -> std::io::Result<()> {
-    let user_key: Key = match get_user_key(gpg_id) {
-        Some(key) => key,
-        None => {
-            println!("Key for {} not found! Consider generating one.", gpg_id);
-            return Ok(());
-        }
+#[derive(Args, Debug)]
+struct EditCommand {
+    pass_name: String,
+}
+
+#[derive(Args, Debug)]
+struct InsertCommand {
+    pass_name: String,
+    #[arg(short, long)]
+    force: bool,
+}
+
+#[derive(Args, Debug)]
+struct LSCommand {
+    subfolder: Option<String>,
+}
+
+#[derive(Args, Debug)]
+struct FindCommand {
+    pass_names: Vec<String>,
+}
+
+#[derive(Args,Debug)]
+struct ShowCommand {
+    pass_name: String,
+    #[arg(short, long)]
+    clip: bool
+}
+
+fn main() {
+    let args = RpassArgs::parse();
+
+    let subfolder = match args {
+        _ => "".to_string(),
     };
 
-    assert!(user_key.is_qualified());
+    let rpassword_store_dir = build_password_dir(subfolder);
+    let mut manager = match rpass::RpassManager::new(rpassword_store_dir) {
+        Ok(m) => m,
+        Err(err) => { eprintln!("{}",err); return; }
+    };
 
-    let mut path = store_dir;
-    create_dir_all(&path)?;
-    path.push(".gpg-id");
-    let mut file = File::create(&path)?;
-    file.write_all(gpg_id.as_bytes())?;
-    Ok(())
-}
+    // Sub Command dispatch.
+    let command_result = match args.command {
+        RpassCommand::Init(command) => init(&command.gpg_id),
+        RpassCommand::Ls(_) => ls(&manager),
+        RpassCommand::Find(command) => find(&manager, command.pass_names),
+        RpassCommand::Show(command) => show(&mut manager, command.pass_name, command.clip),
+        RpassCommand::Insert(command) => insert(&mut manager, command.pass_name, command.force),
+        RpassCommand::Edit(command) => edit(&mut manager, command.pass_name),
+    };
 
-fn get_user_key(username: &str) -> Option<Key> {
-    let mut mode = KeyListMode::empty();
-    mode.insert(KeyListMode::LOCAL);
-
-    let mut ctx = match Context::from_protocol(Protocol::OpenPgp) {
-        Ok(it) => it,
+    // Error Handling
+    match command_result {
+        Ok(()) => (),
         Err(err) => {
-            println!("{:?}", err);
-            return None;
-        }
-    };
-    match ctx.set_key_list_mode(mode) {
-        Ok(it) => it,
-        Err(err) => {
-            println!("{:?}", err);
-            return None;
-        }
-    };
-    let mut keys = match ctx.secret_keys() {
-        Ok(it) => it,
-        Err(err) => {
-            println!("{:?}", err);
-            return None;
-        }
-    };
-    for key in keys.by_ref().filter_map(|x| x.ok()) {
-        // FIXME
-        let name = key.user_ids().last().unwrap().address().unwrap();
-        if username == name {
-            return Some(key);
+            eprintln!("{}", err);
         }
     }
-    None
 }
 
-
-fn find(search_terms: Vec<String>, store_dir: PathBuf) -> std::io::Result<()> {
-    let regexes: Vec<_> = search_terms.iter()
-        .map(|pass_name| format!(r".*{}.*", pass_name))
-        .collect();
-    let regex_set = RegexSet::new(regexes).unwrap();
-    let password_names = read_password_names(&store_dir)?;
-
-    println!("Search Terms: {}",search_terms.join(","));
-    for pw in password_names {
-        if regex_set.matches(&pw).into_iter().count() > 0 {
-            println!("|-- {}",pw);
-        }
-    }
-    Ok(())
-}
 
 fn build_password_dir(subfolder: String) -> PathBuf {
     let default_store_dir = home_dir().unwrap().as_path().join(RPASS_DEFAULT_STORE_NAME);
@@ -129,79 +118,129 @@ fn build_password_dir(subfolder: String) -> PathBuf {
     rpassword_store_dir
 }
 
-#[derive(Debug, Parser)]
-#[clap(author, version, about)]
-pub struct RpassArgs {
-    #[clap(subcommand)]
-    pub command: RpassCommand,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum RpassCommand {
-    /// Initialize new password storage and use gpg-id for encryption.
-    /// Selectively reencrypt existing passwords using new gpg-id.
-    Init(InitCommand),
-    /// List passwords.
-    Ls(LSCommand),
-    /// List passwords that match pass-names.
-    Find(FindCommand),
-}
-
-#[derive(Args, Debug)]
-pub struct InitCommand {
-    #[arg(short, long)]
-    pub subfolder: Option<String>,
-
-    pub gpg_id: String,
-}
-
-#[derive(Args, Debug)]
-pub struct LSCommand {
-    pub subfolder: Option<String>,
-}
-
-#[derive(Args, Debug)]
-pub struct FindCommand {
-    pub pass_names: Vec<String>,
-}
-
-fn main() {
-    let args = RpassArgs::parse();
-
-    let subfolder = match args {
-        _ => "".to_string(),
-    };
-
-    let rpassword_store_dir = build_password_dir(subfolder);
-
-    let command_result = match args.command {
-        RpassCommand::Init(command) => init(&command.gpg_id, rpassword_store_dir),
-        RpassCommand::Ls(_) => ls(rpassword_store_dir),
-        RpassCommand::Find(command) => find(command.pass_names, rpassword_store_dir),
-    };
-
-    match command_result {
-        Ok(()) => (),
-        Err(err) => {
-            eprintln!("{}", err);
-        }
-    }
-}
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_get_user_key() {
-        assert!(get_user_key("janrossbach3@gmail.com").is_some());
-        assert!(get_user_key("").is_none());
-    }
 
     #[test]
     fn test_build_password_dir() {
         assert_eq!(build_password_dir("".to_string()).to_str().unwrap(), "/home/jan/repositories/rpass/store/");
         assert_eq!(build_password_dir("jan".to_string()).to_str().unwrap(), "/home/jan/repositories/rpass/store/jan");
     }
+}
+
+
+fn ls(manager: &RpassManager) -> io::Result<()> {
+    let pw_names = manager.get_password_names()?;
+    println!("Password store:");
+    for pw in pw_names {
+        println!("|-- {}", pw);
+    }
+    Ok(())
+}
+
+// TODO
+fn init(_gpg_id: &str) -> io::Result<()> {
+    // let user_key: Key = match get_user_key(gpg_id)? {
+    //     Some(key) => key,
+    //     None => {
+    //         println!("Key for {} not found! Consider generating one.", gpg_id);
+    //         return Ok(());
+    //     }
+    // };
+
+    // assert!(user_key.is_qualified());
+
+    // let mut path = store_dir.clone();
+    // create_dir_all(&path)?;
+    // path.push(".gpg-id");
+    // let mut file = File::create(&path)?;
+    // file.write_all(gpg_id.as_bytes())?;
+    Ok(())
+}
+
+fn insert(manager: &mut RpassManager, pass_name:String, force: bool) -> std::io::Result<()> {
+    if manager.pass_exists(pass_name.clone()) && !force {
+        if prompt_user("Password already exists. Override?") {
+            println!("Overriding.");
+        } else {
+            println!("Abort.");
+            return Ok(());
+        }
+    }
+
+    let password = get_pass_from_user()?;
+    manager.save_password(pass_name, password)
+}
+
+fn find(manager: &RpassManager, search_terms: Vec<String>) -> std::io::Result<()> {
+    let regexes: Vec<_> = search_terms.iter()
+        .map(|pass_name| format!(r".*{}.*", pass_name))
+        .collect();
+    let regex_set = RegexSet::new(regexes).unwrap();
+    let password_names = manager.get_password_names()?;
+
+    println!("Search Terms: {}",search_terms.join(","));
+    for pw in password_names {
+        if regex_set.matches(&pw).into_iter().count() > 0 {
+            println!("|-- {}",pw);
+        }
+    }
+    Ok(())
+}
+
+fn show(manager: &mut RpassManager, pass_name: String, clip: bool) -> std::io::Result<()> {
+
+    let password = manager.get_password(pass_name)?;
+
+    if clip {
+        println!("Password put into clipboard!"); // FIXME
+    } else {
+        println!("{}", password);
+    }
+    Ok(())
+}
+
+fn edit(manager: &mut RpassManager, pass_name: String) -> std::io::Result<()> {
+
+    if manager.pass_exists(pass_name.clone()) {
+
+        let tmp_file: String = format!("/tmp/{}pwtempfile",pass_name);
+        Command::new("vim")
+            .arg(tmp_file.clone())
+            .status()
+            .expect("Failed to edit file in vim.");
+        let mut file = File::open(tmp_file)?;
+        let mut new_password = String::new();
+        file.read_to_string(&mut new_password)?;
+
+        manager.save_password(pass_name, new_password)?;
+        return Ok(());
+    }
+
+    let password = get_pass_from_user()?;
+    manager.save_password(pass_name, password)
+}
+
+fn get_pass_from_user() -> std::io::Result<String> {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    Ok(input)
+}
+
+fn prompt_user(message: &str) -> bool {
+    println!("{} [y/n] ", message);
+    let mut answer: [u8; 1] = [0];
+    std::io::stdin().read_exact(&mut answer).unwrap();
+    let res = match answer[0] as char {
+        'y' => true,
+        _ => false
+    };
+    // For some reason, after reading in with read_exact, rust just ignores the
+    // next call to read_line. So here I just use one so it does not disrupt the
+    // rest of the program. This seems like a bug...
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    res
 }
---
+++ src/rpass.rs
@@ -0,0 +1,99 @@
+use std::collections::HashSet;
+use std::io::{self, Read};
+use std::fs::{File, self};
+use std::path::PathBuf;
+
+use gpgme::{Key, Context, Protocol, KeyListMode};
+
+const GPG_ID_FILE_NAME: &str = "/.gpg-id";
+
+pub struct RpassManager {
+    store_dir: PathBuf,
+    key: Key,
+    context: Context
+}
+
+impl RpassManager {
+
+    pub fn new(store_dir: PathBuf) -> io::Result<Self> {
+        let gpg_id = read_gpg_id(&store_dir)?;
+        let key = get_user_key(&gpg_id)?.unwrap();
+        let context = Context::from_protocol(Protocol::OpenPgp)?;
+        Ok(RpassManager {
+            store_dir,
+            key,
+            context
+        })
+    }
+
+    pub fn pass_exists(self: &Self, pass_name: String) -> bool {
+        let file = self.pass_to_file(pass_name);
+        file.exists()
+    }
+
+    /// Takes a clearstring password, encrypts it and saves it to the filesystem.
+    /// The filepath will be store_dir/pass_name.gpg
+    pub fn save_password(self: &mut Self, pass_name: String, password: String) -> io::Result<()> {
+        let filename = self.pass_to_file(pass_name);
+        let mut output = File::create(filename)?;
+        self.context.set_armor(true);
+        self.context.encrypt(vec![&self.key], password, &mut output)?;
+        Ok(())
+    }
+
+    /// Fetches all the password names in the store. Meaning files with the .gpg extension.
+    pub fn get_password_names(self: &Self) -> io::Result<HashSet<String>> {
+        let mut passwords: HashSet<String> = HashSet::new();
+        for dir_entry in fs::read_dir(&self.store_dir)? {
+            let dir_entry = dir_entry?;
+            let dir_entry = dir_entry.path();
+            if let Some(ext) = dir_entry.extension() {
+                if ext == "gpg" {
+                    let file_name = dir_entry.file_stem().unwrap();
+                    let file_name = file_name.to_str().unwrap();
+                    passwords.insert(file_name.to_string());
+                }
+            }
+        }
+        Ok(passwords)
+    }
+
+    /// Returns the decrypted cleartext password with the given name as a Result.
+    pub fn get_password(self: &mut Self, pass_name: String) -> io::Result<String> {
+        let filename = self.pass_to_file(pass_name);
+        let mut input = File::open(&filename)?;
+        let mut output = Vec::new();
+        self.context.decrypt(&mut input, &mut output)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, format!("{:?}",e)))?;
+        let result = std::str::from_utf8(&output).unwrap();
+        Ok(result.trim().to_string())
+    }
+
+    pub fn pass_to_file(self: &Self,pass_name: String) -> PathBuf {
+        self.store_dir.clone().join(pass_name + ".gpg")
+    }
+}
+
+fn read_gpg_id(store_dir: &PathBuf) -> std::io::Result<String> {
+    let gpg_id_filename = store_dir.to_str().unwrap().to_string() + GPG_ID_FILE_NAME;
+    let mut gpg_id_file = fs::File::open(gpg_id_filename)?;
+    let mut gpg_id = String::new();
+    gpg_id_file.read_to_string(&mut gpg_id)?;
+    return Ok(gpg_id);
+}
+
+pub fn get_user_key(username: &str) -> std::io::Result<Option<Key>> {
+    let mut mode = KeyListMode::empty();
+    mode.insert(KeyListMode::LOCAL);
+
+    let mut ctx = Context::from_protocol(Protocol::OpenPgp)?;
+    ctx.set_key_list_mode(mode)?;
+    let mut keys = ctx.secret_keys()?;
+    for key in keys.by_ref().filter_map(|x| x.ok()) {
+        let name = key.user_ids().last().unwrap().address().unwrap();
+        if username == name {
+            return Ok(Some(key));
+        }
+    }
+    Ok(None)
+}
