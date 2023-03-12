15f51a0286cd8fe43944e1c1b63b1e3c3b7a93f5
JanRossbach <janrossbach3@gmail.com>
2023-03-12 13:50:46 +0000

    add: Generate rm/mv/cp

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::{Args, Parser, Subcommand};
+use gpgme::Key;
+use passwords::PasswordGenerator;
 use regex::RegexSet;
 use rpass::RpassManager;
 
-use std::{str::FromStr, env, process::Command, io::{self, Read}, fs::File};
+use std::{str::FromStr, env, process::{Command, Stdio}, io::{self, Read, Write}, fs::{File, create_dir_all}, thread::sleep, time::Duration};
 
 use home::home_dir;
 
@@ -36,6 +38,29 @@ enum RpassCommand {
     Insert(InsertCommand),
     /// Insert a new password or edit an existing password using your $EDITOR or neovim as a fallback.
     Edit(EditCommand),
+    /// Generate a new password of pass-length (or 25 if unspecified) with optionally no symbols.
+    /// Optionally put it on the clipboard and clear board after 45 seconds.
+    /// Prompt before overwriting existing password unless forced.
+    /// optionally replace only the first line of an existing file with a new password.
+    Generate(GenerateCommand),
+    /// Remove existing password or directory, optionally forcefully.
+    Rm(RemoveCommand),
+    /// Copies old-path to new-path, optionally forcefully, selectively reencrypting.
+    Cp(CopyCommand),
+    /// Renames or moves old-path to new-path, optionally forcefully, selectively reencrypting.
+    Mv(MoveCommand),
+}
+
+#[derive(Args, Debug)]
+struct GenerateCommand {
+    #[arg(short, long)]
+    no_symbols: bool,
+    #[arg(short, long)]
+    clip: bool,
+    #[arg(short, long)]
+    force: bool,
+    pass_name: String,
+    pass_length: Option<usize>,
 }
 
 #[derive(Args, Debug)]
@@ -44,6 +69,31 @@ struct InitCommand {
     subfolder: Option<String>,
 
     gpg_id: String,
+}
+
+#[derive(Args, Debug)]
+struct MoveCommand {
+    old_name: String,
+    new_name: String,
+    #[arg(short, long)]
+    force: bool,
+}
+
+#[derive(Args, Debug)]
+struct RemoveCommand {
+    pass_name: String,
+    #[arg(short, long)]
+    force: bool,
+    #[arg(short, long)]
+    recursive: bool,
+}
+
+#[derive(Args, Debug)]
+struct CopyCommand {
+    old_name: String,
+    new_name: String,
+    #[arg(short, long)]
+    force: bool,
 }
 
 #[derive(Args, Debug)]
@@ -83,19 +133,40 @@ fn main() {
     };
 
     let rpassword_store_dir = build_password_dir(subfolder);
+
+
+    // When calling init for the first time, we short circuit here before createing the pw-manager.
+    if !rpassword_store_dir.exists() {
+        match args.command {
+            RpassCommand::Init(command) => {
+                let result = init_new(&command.gpg_id, &rpassword_store_dir);
+                match result {
+                    Ok(()) => println!("Password Store successfully created at {:?}", rpassword_store_dir),
+                    Err(err) => println!("Password Store create failed because: {}", err),
+                };
+                return;
+            }
+            _ => ()
+        }
+    }
+
     let mut manager = match rpass::RpassManager::new(rpassword_store_dir) {
         Ok(m) => m,
-        Err(err) => { eprintln!("{}",err); return; }
+        Err(err) => { eprintln!("Password Manager could not be loaded because: {}",err); return; }
     };
 
     // Sub Command dispatch.
     let command_result = match args.command {
-        RpassCommand::Init(command) => init(&command.gpg_id),
+        RpassCommand::Init(command) => init_reencrypt(&manager, &command.gpg_id),
         RpassCommand::Ls(_) => ls(&manager),
         RpassCommand::Find(command) => find(&manager, command.pass_names),
         RpassCommand::Show(command) => show(&mut manager, command.pass_name, command.clip),
         RpassCommand::Insert(command) => insert(&mut manager, command.pass_name, command.force),
         RpassCommand::Edit(command) => edit(&mut manager, command.pass_name),
+        RpassCommand::Generate(command) => generate(&mut manager, command),
+        RpassCommand::Mv(command) => move_password(&mut manager, command),
+        RpassCommand::Cp(command) => copy_password(&mut manager, command),
+        RpassCommand::Rm(command) => remove_password(&mut manager, command),
     };
 
     // Error Handling
@@ -107,6 +178,78 @@ fn main() {
     }
 }
 
+fn move_password(manager: &mut RpassManager, command: MoveCommand) -> io::Result<()> {
+
+    let src_file = manager.pass_to_file(command.old_name);
+    let target_file = manager.pass_to_file(command.new_name);
+
+    let output = Command::new("mv")
+        .arg(src_file)
+        .arg(target_file)
+        .arg("-v")
+        .output()
+        .expect("Failed to edit file in vim.");
+
+    let result = std::str::from_utf8(&output.stdout);
+    println!("{}",result.unwrap().trim());
+    Ok(())
+}
+
+fn copy_password(manager: &mut RpassManager, command: CopyCommand) -> io::Result<()> {
+    let src_file = manager.pass_to_file(command.old_name);
+    let target_file = manager.pass_to_file(command.new_name);
+
+    let output = Command::new("cp")
+        .arg(src_file)
+        .arg(target_file)
+        .arg("-v")
+        .output()
+        .expect("Failed to edit file in vim.");
+
+    let result = std::str::from_utf8(&output.stdout);
+    println!("{}",result.unwrap().trim());
+    Ok(())
+}
+
+fn remove_password(manager: &mut RpassManager,command: RemoveCommand) -> io::Result<()> {
+    let file = manager.pass_to_file(command.pass_name.clone());
+
+    let prompt = format!("Are you sure you would like to delete {}?", command.pass_name);
+
+    // TODO subfolders
+
+    if !manager.pass_exists(command.pass_name.clone()) {
+        eprintln!("Password {} does not exist.",command.pass_name);
+        return Ok(());
+    }
+
+    if !command.force {
+        if prompt_user(&prompt) {
+            println!("Deleting");
+        } else {
+            println!("Abort");
+            return Ok(());
+        }
+    }
+
+    let mut output = Command::new("rm");
+    output.arg("-v");
+    output.arg(file);
+
+    if command.force {
+        output.arg("-f");
+    }
+
+    if command.recursive {
+        output.arg("-r");
+    }
+
+    let output = output.output().expect("Failed to remove Password File.");
+
+    let result = std::str::from_utf8(&output.stdout);
+    println!("{}",result.unwrap().trim());
+    Ok(())
+}
 
 fn build_password_dir(subfolder: String) -> PathBuf {
     let default_store_dir = home_dir().unwrap().as_path().join(RPASS_DEFAULT_STORE_NAME);
@@ -129,7 +272,6 @@ mod tests {
     }
 }
 
-
 fn ls(manager: &RpassManager) -> io::Result<()> {
     let pw_names = manager.get_password_names()?;
     println!("Password store:");
@@ -139,27 +281,31 @@ fn ls(manager: &RpassManager) -> io::Result<()> {
     Ok(())
 }
 
-// TODO
-fn init(_gpg_id: &str) -> io::Result<()> {
-    // let user_key: Key = match get_user_key(gpg_id)? {
-    //     Some(key) => key,
-    //     None => {
-    //         println!("Key for {} not found! Consider generating one.", gpg_id);
-    //         return Ok(());
-    //     }
-    // };
+fn init_new(gpg_id: &str, store_dir: &PathBuf) -> io::Result<()> {
+    let user_key: Key = match rpass::get_user_key(gpg_id)? {
+        Some(key) => key,
+        None => {
+            println!("Key for {} not found! Consider generating one.", gpg_id);
+            return Ok(());
+        }
+    };
 
-    // assert!(user_key.is_qualified());
+    assert!(user_key.is_qualified());
 
-    // let mut path = store_dir.clone();
-    // create_dir_all(&path)?;
-    // path.push(".gpg-id");
-    // let mut file = File::create(&path)?;
-    // file.write_all(gpg_id.as_bytes())?;
+    let mut path = store_dir.clone();
+    create_dir_all(&path)?;
+    path.push(".gpg-id");
+    let mut file = File::create(&path)?;
+    file.write_all(gpg_id.as_bytes())?;
     Ok(())
 }
 
-fn insert(manager: &mut RpassManager, pass_name:String, force: bool) -> std::io::Result<()> {
+fn init_reencrypt(_manager: &RpassManager,_gpg_id: &str) -> io::Result<()> {
+    // TODO
+    todo!();
+}
+
+fn insert(manager: &mut RpassManager, pass_name:String, force: bool) -> io::Result<()> {
     if manager.pass_exists(pass_name.clone()) && !force {
         if prompt_user("Password already exists. Override?") {
             println!("Overriding.");
@@ -173,7 +319,7 @@ fn insert(manager: &mut RpassManager, pass_name:String, force: bool) -> std::io:
     manager.save_password(pass_name, password)
 }
 
-fn find(manager: &RpassManager, search_terms: Vec<String>) -> std::io::Result<()> {
+fn find(manager: &RpassManager, search_terms: Vec<String>) -> io::Result<()> {
     let regexes: Vec<_> = search_terms.iter()
         .map(|pass_name| format!(r".*{}.*", pass_name))
         .collect();
@@ -189,19 +335,38 @@ fn find(manager: &RpassManager, search_terms: Vec<String>) -> std::io::Result<()
     Ok(())
 }
 
-fn show(manager: &mut RpassManager, pass_name: String, clip: bool) -> std::io::Result<()> {
+fn show(manager: &mut RpassManager, pass_name: String, clip: bool) -> io::Result<()> {
 
-    let password = manager.get_password(pass_name)?;
+    let password = manager.get_password(pass_name.clone())?;
 
     if clip {
-        println!("Password put into clipboard!"); // FIXME
+        into_clipboard(password)?;
+        println!("Copied {} to clipboard. Will clear in 45 seconds.", pass_name);
+        sleep(Duration::from_millis(45000));
+        into_clipboard("".to_string())?;
+        println!("Clipboard cleared.");
     } else {
         println!("{}", password);
     }
     Ok(())
 }
 
-fn edit(manager: &mut RpassManager, pass_name: String) -> std::io::Result<()> {
+fn into_clipboard(output: String) -> io::Result<()> {
+    let mut xclip = Command::new("xclip")
+        .arg("-selection")
+        .arg("clipboard")
+        .arg("-i")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    let xclip_stdin = xclip.stdin.as_mut().unwrap();
+    xclip_stdin.write_all(output.as_bytes())?;
+    drop(xclip_stdin);
+    xclip.wait()?;
+    Ok(())
+}
+
+fn edit(manager: &mut RpassManager, pass_name: String) -> io::Result<()> {
 
     if manager.pass_exists(pass_name.clone()) {
 
@@ -222,7 +387,45 @@ fn edit(manager: &mut RpassManager, pass_name: String) -> std::io::Result<()> {
     manager.save_password(pass_name, password)
 }
 
-fn get_pass_from_user() -> std::io::Result<String> {
+fn generate(manager: &mut RpassManager, command: GenerateCommand) -> io::Result<()> {
+    let pg = PasswordGenerator {
+        length: command.pass_length.unwrap_or(25),
+        numbers: true,
+        lowercase_letters: true,
+        uppercase_letters: true,
+        symbols: !command.no_symbols,
+        spaces: false,
+        exclude_similar_characters: false,
+        strict: true,
+    };
+
+    let pass_name = command.pass_name.clone();
+    if manager.pass_exists(pass_name.clone()) && !command.force {
+        if prompt_user("Password already exists. Override?") {
+            println!("Overriding.");
+        } else {
+            println!("Abort.");
+            return Ok(());
+        }
+    }
+
+    let password = pg.generate_one().unwrap();
+    manager.save_password(command.pass_name, password.clone())?;
+
+    if command.clip {
+        into_clipboard(password.clone())?;
+        println!("Copied {} to clipboard. Will clear in 45 seconds.", pass_name);
+        sleep(Duration::from_millis(45000));
+        into_clipboard("".to_string())?;
+        println!("Clipboard cleared.");
+    } else {
+        println!("{}", password);
+    }
+
+    Ok(())
+}
+
+fn get_pass_from_user() -> io::Result<String> {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input)?;
     Ok(input)
